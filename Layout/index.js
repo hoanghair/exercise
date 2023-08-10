37285b6fb5bfa9d2dab2e12b384bad1b45e5a9f9
hainh <hoanghai16072000@gmail.com>
2023-08-10 17:05:48 +0700

    fix layout

--- Layout/index.js
+++ Layout/index.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
-  $(".btn__navbar").click(function () {
+  function onNavBar() {
     if ($(this).hasClass("btn__navbar-on")) {
-      $(".nav").show();
+      $(".nav").css("display", "block");
       $(this).removeClass("btn__navbar-on").addClass("btn__navbar-off");
     } else {
-      $(".nav").hide();
+      $(".nav").css("display", "none");
       $(this).removeClass("btn__navbar-off").addClass("btn__navbar-on");
     }
-  });
+  }
+
+  $(".btn__navbar").on("click", onNavBar);
 });
+$(window).on("resize", function () {
+  if($(window).width() > 768){
+    $(".nav").addClass("is-on")
+  }else{
+    $(".nav").removeClass("is-on")
+  }
+});
+
